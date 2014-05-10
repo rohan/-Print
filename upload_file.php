0de0a6238742c6eeba74b19b65622b08e2aae401
Rohan Ramchand <rohan@cs.utexas.edu>
2014-05-09 23:39:44 -0500

    test

--- upload_file.php
+++ upload_file.php
@@ -3,7 +3,7 @@ $allowedExts = array("pdf", "txt");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
 
-echo "test" . $_FILES["file"]["name"];
+echo "test " . $extension . "\n";
 
 if ((in_array($extension, $allowedExts))) {
   if ($_FILES["file"]["error"] > 0) {
@@ -13,13 +13,14 @@ if ((in_array($extension, $allowedExts))) {
     echo "Type: " . $_FILES["file"]["type"] . "<br>";
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-    if (file_exists("upload/" . $_FILES["file"]["name"])) {
-      echo $_FILES["file"]["name"] . " already exists. ";
-    } else {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+    $counter = 1;
+    $name = "upload/" . $_FILES["file"]["name"];
+    while (file_exists($name)) {
+        $name = "upload/" . $_FILES["file"]["name"] . "_" . $counter;
+        $counter++;
     }
+  move_uploaded_file($_FILES["file"]["tmp_name"], $name);
+  echo "Stored in: " . $name;
   }
 } else {
   echo "Invalid file";
