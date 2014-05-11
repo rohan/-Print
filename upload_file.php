36a629c5baafd5ba31b47d0231a44f5afe240675
Rohan Ramchand <rohan@cs.utexas.edu>
2014-05-11 00:08:13 -0500

    mucking with PHP

--- upload_file.php
+++ upload_file.php
@@ -3,13 +3,13 @@ $allowedExts = array("PDF", "ASCII");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
 
-$name = $_FILES["file"]["name"];
+$name = $_FILES["file"]["tmp_name"];
 
 if (($_FILES["file"]["size"] / 1024) <= 10000) {
 
     $validExt = FALSE;
 
-    echo $name . " " . shell_exec("file upload/" . $name) . " ";
+    echo $name . " " . shell_exec("file ". $name) . " ";
     foreach ($allowedExts as $ext) {
         if (strpos(shell_exec("file " . $name), $ext) !== FALSE) {
             $validExt = TRUE;
