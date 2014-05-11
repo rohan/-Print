5d9b8971fdcb2b3b281b821ccc610a251c54d92c
Rohan Ramchand <rohan@cs.utexas.edu>
2014-05-11 00:09:19 -0500

    mucking with PHP

--- upload_file.php
+++ upload_file.php
@@ -9,7 +9,6 @@ if (($_FILES["file"]["size"] / 1024) <= 10000) {
 
     $validExt = FALSE;
 
-    echo $name . " " . shell_exec("file ". $name) . " ";
     foreach ($allowedExts as $ext) {
         if (strpos(shell_exec("file " . $name), $ext) !== FALSE) {
             $validExt = TRUE;
@@ -17,15 +16,12 @@ if (($_FILES["file"]["size"] / 1024) <= 10000) {
         }
     }
 
-    echo $validExt;
-
     if ($validExt == FALSE) {
-        echo "poop";
-        //header("Location: http://meru.noip.me/failure.html");
+        header("Location: http://meru.noip.me/failure.html");
     }
 
     if ($_FILES["file"]["error"] > 0) {
-        //header("Location: http://meru.noip.me/failure.html");
+        header("Location: http://meru.noip.me/failure.html");
     } else {
         $counter = 1;
         $name = "upload/" . $_FILES["file"]["name"];
@@ -33,10 +29,10 @@ if (($_FILES["file"]["size"] / 1024) <= 10000) {
             $name = "upload/" . $_FILES["file"]["name"] . "_" . $counter;
             $counter++;
         }
-        //move_uploaded_file($_FILES["file"]["tmp_name"], $name);
-        //header("Location: http://meru.noip.me/success.html");
+        move_uploaded_file($_FILES["file"]["tmp_name"], $name);
+        header("Location: http://meru.noip.me/success.html");
     }
 } else {
-    //header("Location: http://meru.noip.me/failure.html");
+    header("Location: http://meru.noip.me/failure.html");
 }
 ?>
